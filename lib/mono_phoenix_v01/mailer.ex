084b853f8036ff1f58f7090c4afc7c4a82494d12
Steven Shults <steven@shults.org>
2023-01-25 23:47:56 -0800

    First commit. All the things.

---
+++ lib/mono_phoenix_v01/mailer.ex
@@ -0,0 +1,3 @@
+defmodule MonoPhoenixV01.Mailer do
+  use Swoosh.Mailer, otp_app: :mono_phoenix_v01
+end
---
+++ lib/mono_phoenix_v01_web/controllers/page_controller.ex
@@ -0,0 +1,17 @@
+defmodule MonoPhoenixV01Web.PageController do
+  use MonoPhoenixV01Web, :controller
+  import Ecto.Query, only: [from: 2]
+
+  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
+  def index(conn, _params) do
+    query = from p in "plays",
+      join: m in "monologues",
+      on: m.play_id == p.id,
+      group_by: [p.title, m.character, m.first_line, m.location],
+      select: %{play: p.title, character: m.character, firstline: m.first_line, location: m.location}
+
+      rows = MonoPhoenixV01.Repo.all(query)
+
+      render(conn, "index.html", rows: rows)
+  end
+end
