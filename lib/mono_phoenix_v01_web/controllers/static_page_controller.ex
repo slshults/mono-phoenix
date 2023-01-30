4d3d3e09c6aa55a0757a0114fc35f6322a86d631
Steven Shults <steven@shults.org>
2023-01-29 18:22:56 -0800

    Moved header and footer for statict pages into `templates/layout/root.html.heex`

--- lib/mono_phoenix_v01_web/controllers/static_page_controller.ex
+++ lib/mono_phoenix_v01_web/controllers/static_page_controller.ex
@@ -2,26 +2,32 @@ defmodule MonoPhoenixV01Web.StaticPageController do
   use MonoPhoenixV01Web, :controller
 
   def home(conn, _params) do
-    render(conn, "home.html")
+    page_title = "Welcome"
+    render(conn, "home.html", page_title: page_title)
   end
 
   def faq(conn, _params) do
-    render(conn, "faq.html")
+    page_title = "Frequently Asked Questions"
+    render(conn, "faq.html", page_title: page_title)
   end
 
   def aboutus(conn, _params) do
-    render(conn, "aboutus.html")
+    page_title = "About Us"
+    render(conn, "aboutus.html", page_title: page_title)
   end
 
   def links(conn, _params) do
-    render(conn, "links.html")
+    page_title = "Reciprocal Links"
+    render(conn, "links.html", page_title: page_title)
   end
 
   def privacy(conn, _params) do
-    render(conn, "privacy.html")
+    page_title = "Privacy"
+    render(conn, "privacy.html", page_title: page_title)
   end
 
   def maintenance(conn, _params) do
-    render(conn, "maintenance.html")
+    page_title = "Our Apologies. We'll be back."
+    render(conn, "maintenance.html", page_title: page_title)
   end
 end
