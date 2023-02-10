b7d6b0f37ef806c5cbea8fc1d60464fe6066ba89
Steven Shults <steven@shults.org>
2023-02-09 21:08:36 -0800

    Added files for monologues and plays pages. Not all working yet, but adding as placeholders. It does compile and run.

--- lib/mono_phoenix_v01_web/controllers/page_controller.ex
+++ lib/mono_phoenix_v01_web/controllers/page_controller.ex
@@ -23,30 +23,6 @@ defmodule MonoPhoenixV01Web.PageController do
     render(conn, "index.html", rows: rows)
   end
 
-  # Show all the monologues in the database
-  @spec monologues(Plug.Conn.t(), any) :: Plug.Conn.t()
-  def monologues(conn, _params) do
-    query =
-      from(m in "monologues",
-        join: p in "plays",
-        on: m.play_id == p.id,
-        group_by: [p.title, m.character, m.first_line, m.location],
-        select: %{
-          play: p.title,
-          character: m.character,
-          firstline: m.first_line,
-          location: m.location,
-          body: m.body
-        }
-      )
-
-    rows = MonoPhoenixV01.Repo.all(query)
-
-    rows = MonoPhoenixV01.Repo.all(query)
-
-    render(conn, "monologues.html", rows: rows)
-  end
-
   def sandbox(conn, _params) do
     render(conn, "sandbox.html")
   end
