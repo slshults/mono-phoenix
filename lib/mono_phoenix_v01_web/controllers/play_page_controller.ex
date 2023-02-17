76d30fb2749e30544699144f1d751de5102d6de5
Steven Shults <steven@shults.org>
2023-02-17 01:38:02 -0800

    lots of bootstrappy css layout work. almost there.

--- lib/mono_phoenix_v01_web/controllers/play_page_controller.ex
+++ lib/mono_phoenix_v01_web/controllers/play_page_controller.ex
@@ -12,12 +12,22 @@ defmodule MonoPhoenixV01Web.PlayPageController do
         join: p in "plays",
         on: m.play_id == p.id,
         where: p.id == ^playid,
-        group_by: [p.id, p.title, m.character, m.first_line, m.location, m.body, m.pdf_link],
+        group_by: [
+          p.id,
+          p.title,
+          m.location,
+          m.character,
+          m.first_line,
+          m.style,
+          m.body,
+          m.pdf_link
+        ],
         select: %{
           play: p.title,
+          location: m.location,
+          style: m.style,
           character: m.character,
           firstline: m.first_line,
-          location: m.location,
           body: m.body,
           pdf: m.pdf_link
         }
