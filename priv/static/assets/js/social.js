cb3c3b7559ac6a19dacd3ed4b8ba93167710329c
Steven Shults <steven@shults.org>
2023-02-21 17:26:03 -0800

    More layout fixes. Replaced twitter links with mastodon links. Updated some copy on some of the static pages.

--- priv/static/assets/js/social.js
+++ priv/static/assets/js/social.js
@@ -1,10 +1,3 @@
-/* begin G+ mobile content rec stuff */
-  (function() {
-    var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;
-    po.src = "https://apis.google.com/js/plusone.js?publisherid=106045313819632308693";
-    var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);
-  })();
-/* end G+ mobile content rec stuff */
 
 /* begin everbutton script */
 var EB = EB || {};
@@ -34,17 +27,6 @@ EB.load_buttons();
 }
 /* end everbutton script */
 
-/* begin twitter follow button */
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-/* end twitter follow button */
-
-/* begin G+ button script */
-(function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-/* end G+ button script */
 
 /* begin AddThis button script */
 
