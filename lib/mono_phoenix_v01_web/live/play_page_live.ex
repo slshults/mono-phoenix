46b62b7177f90ae447e7f5c9d9ecd6efc87d7d12
Steven Shults <steven@shults.org>
2023-03-19 18:47:26 -0700

    A few more steps toward search nirvana, but not enlightened yet. Just checking this in so I can readily roll back to this point on my local.

--- lib/mono_phoenix_v01_web/live/play_page_live.ex
+++ lib/mono_phoenix_v01_web/live/play_page_live.ex
@@ -1,7 +1,9 @@
 defmodule MonoPhoenixV01Web.PlayPageLive do
   use MonoPhoenixV01Web, :live_view
+  alias MonoPhoenixV01Web.Router.Helpers, as: Routes
   import Ecto.Query
 
+  @impl true
   def mount(%{"playid" => playid_str}, _session, socket) do
     playid = String.to_integer(playid_str)
     rows = fetch_monologues(playid, "")
@@ -11,9 +13,21 @@ defmodule MonoPhoenixV01Web.PlayPageLive do
   # Add this function to handle the search event
   @impl true
   def handle_event("search", %{"search_value" => search_value}, socket) do
+    # Cancel any previous timer
+    if socket.assigns[:search_timer] do
+      Process.cancel_timer(socket.assigns[:search_timer])
+    end
+
+    # Set a new timer and store its reference in the socket
+    timer_ref = Process.send_after(self(), {:search, search_value}, 300)
+    {:noreply, assign(socket, search_timer: timer_ref)}
+  end
+
+  @impl true
+  def handle_info({:search, search_value}, socket) do
     playid = hd(socket.assigns.rows).play_id
     updated_rows = fetch_monologues(playid, search_value)
-    {:noreply, assign(socket, :rows, updated_rows)}
+    {:noreply, assign(socket, rows: updated_rows, search_timer: nil)}
   end
 
   # Update fetch_monologues/1 to fetch_monologues/2 and add search_value as an argument
