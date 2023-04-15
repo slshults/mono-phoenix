1924c809ad5a76ffe8748377f85572559b4ee730
Steven Shults <steven@shults.org>
2023-04-15 00:05:32 -0700

    added a decent looking toggle button for dark mode

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,13 +1,34 @@
-// Function to toggle dark mode
-    // Function to toggle dark mode
-    function toggleDarkMode() {
-      var bodyElement = document.getElementById('body-element');
-      bodyElement.classList.toggle('dark-mode');
+// Function to set dark mode based on the user's preference
+function setDarkModePreference(isDarkMode) {
+  localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
+  var bodyElement = document.body; // Use 'document.body' instead of 'getElementById'
+  var lightModeToggle = document.getElementById('light-mode-toggle');
+  var darkModeToggle = document.getElementById('dark-mode-toggle');
+  if (isDarkMode) {
+    bodyElement.classList.add('dark-mode');
+    lightModeToggle.style.display = 'none';
+    darkModeToggle.style.display = 'block';
+  } else {
+    bodyElement.classList.remove('dark-mode');
+    lightModeToggle.style.display = 'block';
+    darkModeToggle.style.display = 'none';
   }
+}
 
-  // Attach the toggleDarkMode function to the click event of the dark mode icon
-  var darkModeIcon = document.getElementById('dark-mode-icon');
-  if (darkModeIcon) {
-      darkModeIcon.addEventListener('click', toggleDarkMode);
-  }
-  
+// Function to toggle dark mode
+function toggleDarkMode() {
+  // Check if dark mode is currently enabled
+  var isDarkMode = localStorage.getItem('darkMode') === 'true';
+  // Toggle the dark mode preference and update the UI
+  setDarkModePreference(!isDarkMode);
+}
+
+// Attach the toggleDarkMode function to the click event of the dark mode icon
+var darkModeIcon = document.getElementById('dark-mode-icon');
+if (darkModeIcon) {
+  darkModeIcon.addEventListener('click', toggleDarkMode);
+}
+
+// Set the initial dark mode preference based on localStorage
+var initialDarkMode = localStorage.getItem('darkMode') === 'true';
+setDarkModePreference(initialDarkMode);
