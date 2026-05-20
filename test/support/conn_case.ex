2c76c10d4da33cf471c335b51ec221065b413130
slshults <shakes@shults.org>
2026-05-19 17:16:00 -0700

    feat(accounts): include gen.auth injected conn_case.ex additions
    The generator injected register_and_log_in_user helper and
    updated log_in_user signature into ConnCase.
    
    Generated-By: PostHog Code
    Task-Id: 72115778-c6b5-4054-86f0-c988e4c14ce8

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -32,4 +32,45 @@ defmodule MonoPhoenixV01Web.ConnCase do
     |> Phoenix.ConnTest.init_test_session(%{})
     |> Plug.Conn.put_session(:user_token, token)
   end
+
+  @doc """
+  Setup helper that registers and logs in users.
+
+      setup :register_and_log_in_user
+
+  It stores an updated connection and a registered user in the
+  test context.
+  """
+  def register_and_log_in_user(%{conn: conn} = context) do
+    user = MonoPhoenixV01.AccountsFixtures.user_fixture()
+    scope = MonoPhoenixV01.Accounts.Scope.for_user(user)
+
+    opts =
+      context
+      |> Map.take([:token_authenticated_at])
+      |> Enum.into([])
+
+    %{conn: log_in_user(conn, user, opts), user: user, scope: scope}
+  end
+
+  @doc """
+  Logs the given `user` into the `conn`.
+
+  It returns an updated `conn`.
+  """
+  def log_in_user(conn, user, opts \\ []) do
+    token = MonoPhoenixV01.Accounts.generate_user_session_token(user)
+
+    maybe_set_token_authenticated_at(token, opts[:token_authenticated_at])
+
+    conn
+    |> Phoenix.ConnTest.init_test_session(%{})
+    |> Plug.Conn.put_session(:user_token, token)
+  end
+
+  defp maybe_set_token_authenticated_at(_token, nil), do: nil
+
+  defp maybe_set_token_authenticated_at(token, authenticated_at) do
+    MonoPhoenixV01.AccountsFixtures.override_token_authenticated_at(token, authenticated_at)
+  end
 end
